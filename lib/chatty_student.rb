d03158b2cd33e5d04e5772dd20ff65bb427de095
Alex Wilson <fonzmp@gmail.com>
2018-05-21 16:25:56 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,6 +8,7 @@ class ChattyStudent
     super
     "How are you doing today? I'm okay
     , but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoil
-    ers? Okay well let me just tell you who died...\n"  end
+    ers? Okay well let me just tell you who died...\n"
+  end
 
 end
