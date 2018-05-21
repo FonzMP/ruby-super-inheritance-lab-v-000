4d3f85ac44e1841278a7ea55c6890f64792f5cbc
Alex Wilson <fonzmp@gmail.com>
2018-05-21 16:26:08 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,13 +2,4 @@ require "./lib/student.rb"
 
 class ChattyStudent
 
-  attr_reader :hello
-
-  def hello
-    super
-    "How are you doing today? I'm okay
-    , but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoil
-    ers? Okay well let me just tell you who died...\n"
-  end
-
 end
